6de04cfd509e877127ce644cc75351e56a900140
Senzeanb <2037532@apps.nsd.org>
2025-10-22 15:25:41 +0000

    i havent been here for 2 weeks

--- src/main/java/edu/bhscs/App.java
+++ src/main/java/edu/bhscs/App.java
@@ -1,12 +1,12 @@
 package edu.bhscs;
 
 public class App {
-  public void main(String[]arges){
+  public void main(String[] arges) {
     date10_21();
     System.out.println("I am the app");
-
   }
-  public static void date10_21 (){
-System.out.println("Today's date is October 21");
+
+  public static void date10_21() {
+    System.out.println("Today's date is October 21");
   }
 }
